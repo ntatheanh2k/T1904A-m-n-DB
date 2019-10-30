31dba426c410567ca77f4228c4740ed105669d3a
Nguyễn Thế Anh <50062964+ntatheanh2k@users.noreply.github.com>
2019-10-30 09:02:02 +0700

    1

---
+++ Lap2/Sesion22.java
@@ -0,0 +1,10 @@
+package Lap2;
+
+public class Sesion22 {
+    public static void main(String args[]){
+        Session2 s = new Session2();
+        s.showInfo();
+        s.setName("Haha");
+        System.out.println(s.getName());
+    }
+}
