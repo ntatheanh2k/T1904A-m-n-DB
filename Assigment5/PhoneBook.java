74b777e82e24c856548d604be71b8eadc51f534f
Nguyễn Thế Anh <50062964+ntatheanh2k@users.noreply.github.com>
2019-10-30 09:22:29 +0700

    1

---
+++ Assigment5/PhoneBook.java
@@ -0,0 +1,76 @@
+package Assigment5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+public class PhoneBook  extends Phone {
+    public ArrayList<PhoneNumber> PhoneList;
+
+    public PhoneBook() {
+        PhoneList = new ArrayList();
+    }
+
+    @Override
+    public void insertPhone(String name, String phone) {
+        for(PhoneNumber p: PhoneList){
+            if(p.getName().equals(name)){
+                if(!(p.getPhone().contains(phone))){
+                    p.setPhone(p.getPhone() + ":" + phone);
+                    return;
+                }
+                return;
+            }
+        }
+        PhoneNumber phoneNumber = new PhoneNumber(name,phone);
+        PhoneList.add(phoneNumber);
+
+    }
+
+    @Override
+    public void removePhone(String name) {
+        for (PhoneNumber p: PhoneList){
+            if(p.getName().equals(name)){
+                PhoneList.remove(p);
+                return;
+            }
+        }
+
+    }
+
+    @Override
+    public void updatePhone(String name, String newphone) {
+        for(PhoneNumber p:PhoneList){
+            if(p.getName().equals(name)){
+                p.setPhone(newphone);
+                return;
+            }
+        }
+
+    }
+
+    @Override
+    public void searchPhone(String name) {
+        for (PhoneNumber p:PhoneList){
+            if(p.getName().equals(name)){
+                System.out.println(p.getPhone());
+                return;
+            }
+        }
+
+    }
+
+    @Override
+    public void sort() {
+        Collections.sort(PhoneList, new Comparator<PhoneNumber>() {
+            @Override
+            public int compare(PhoneNumber o1, PhoneNumber o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for(PhoneNumber p:PhoneList){
+            System.out.println(p.getName()+"-"+p.getPhone());
+        }
+
+    }
+}
---
+++ Assigment5/PhoneNumber.java
@@ -0,0 +1,31 @@
+package Assigment5;
+
+public class PhoneNumber {
+    public String phone;
+    public String name;
+
+    public PhoneNumber(){
+
+    }
+
+    public PhoneNumber(String phone, String name) {
+        this.phone = phone;
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
