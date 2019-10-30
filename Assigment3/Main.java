0f9967465fae4f56980c1f9ed2f805ba681745fc
Nguyễn Thế Anh <50062964+ntatheanh2k@users.noreply.github.com>
2019-10-30 09:16:06 +0700

    1

---
+++ Assigment3/Main.java
@@ -0,0 +1,10 @@
+package Assigment3;
+
+public class Main {
+    public static void main(String args[]){
+        Student sv1 = new Student();
+        sv1.inputInfo();
+        sv1.showInfo();
+        sv1.xetHocbong();
+    }
+}
