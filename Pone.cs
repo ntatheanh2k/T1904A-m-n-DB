5f0465b88e26055cc885a21bd4f4c0363a6afcd8
Nguyễn Thế Anh <50062964+ntatheanh2k@users.noreply.github.com>
2020-03-02 16:16:00 +0700

    baitap1

---
+++ Pone.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace ConsoleApplication3
+{
+    public class Phone
+    {
+        protected string phonename;
+        protected string phonetype;
+        protected float phoneprice;
+
+        public Phone()
+        {
+        }
+
+        public Phone(string phonename, string phonetype, float phoneprice)
+        {
+            this.phonename = phonename;
+            this.phonetype = phonetype;
+            this.phoneprice = phoneprice;
+        }
+
+        public string PhoneName
+        {
+            get
+            {
+                return phonename;
+            }
+
+            set
+            {
+                phonename = value;
+            }
+        }
+
+        public string PhoneType
+        {
+            get
+            {
+                return phonetype;
+            }
+
+            set
+            {
+                phonetype = value;
+            }
+        }
+
+        public float PhonePrice
+        {
+            get
+            {
+                return phoneprice;
+            }
+
+            set
+            {
+                phoneprice = value;
+            }
+        }
+
+
+        public void display()
+        {
+            Console.WriteLine("Phone " + PhoneName + ", type " + PhoneType + ", price " + PhonePrice);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone testPhone = new Phone("theanh", "display phone", 5686.2f);
+            MobilePhone testMobilePhone = new MobilePhone();
+            testPhone.display();
+            testMobilePhone.display();
+        }
+    }
+}
