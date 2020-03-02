ce5acc57ac7c7491350c5963c56ad9f60e6822ee
Nguyễn Thế Anh <50062964+ntatheanh2k@users.noreply.github.com>
2020-03-02 14:03:52 +0700

    Add files via upload

---
+++ Assignment4/Program.cs
@@ -0,0 +1,50 @@
+﻿sing System;
+using System.Threading;
+
+namespace t1904a.Assignment4
+{
+    public class Program
+    {
+        public static bool flag = false;
+        static void Main(string[] args)
+        {
+            Int32 n = Convert.ToInt32(Console.ReadLine());
+            Thread t1 = new Thread(findFibo);
+            t1.Start(n);
+            Thread t2 = new Thread(count);
+            t2.Start();
+        }
+
+        public static void findFibo(Object n)
+        {
+            Int32 na = Convert.ToInt32(n);
+            int f1 = 1,f2 = 2,fn = 0;
+            while (f1 + f2 < na)
+            {
+                fn = f1 + f2;
+                f1 = f2;
+                f2 = fn;                
+                Thread.Sleep(100);
+
+            }
+            Program.flag = true;
+            Console.WriteLine(fn);
+        }
+        public static void count()
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;
+                if (Program.flag)
+                {
+                    
+                    Console.WriteLine(i*100 + "milis");
+                    return;
+                }
+                Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
