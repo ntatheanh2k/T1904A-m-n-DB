0751ffc1525524e46cb50d4d9014ddd82b9512a5
Nguyễn Thế Anh <50062964+ntatheanh2k@users.noreply.github.com>
2020-03-02 09:22:02 +0700

    Add files via upload

---
+++ Labsession1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Labsession1
+{
+    public class Fibonaci
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = FindFibonaci(n);
+            Console.WriteLine("So nguyen to lon nhat nho hon n la: "+a);
+        }
+        
+        static int FindFibonaci(int x)
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            while (c < x)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return b;
+        }
+    }
+}
